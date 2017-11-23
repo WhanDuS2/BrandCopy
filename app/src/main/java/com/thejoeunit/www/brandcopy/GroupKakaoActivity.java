3a5f9b82af57eee8ca68278bf63ac03695586ff3
cho881020 <cho881020@gmail.com>
2017-11-23 18:01:14 +0900

    줌 라이브러리 투입

--- app/src/main/java/com/thejoeunit/www/brandcopy/GroupKakaoActivity.java
+++ app/src/main/java/com/thejoeunit/www/brandcopy/GroupKakaoActivity.java
@@ -1,9 +1,15 @@
 package com.thejoeunit.www.brandcopy;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.emrekose.pinchzoom.Touch;
 
 public class GroupKakaoActivity extends BaseActivity {
+
+    private android.widget.ImageView imageView2;
+    private android.widget.ImageView imageView;
+    private android.widget.ImageView imageView3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,11 +28,15 @@ public class GroupKakaoActivity extends BaseActivity {
 
     @Override
     public void setValues() {
+        imageView.setOnTouchListener(new Touch());
 
     }
 
     @Override
     public void bindViews() {
 
+        this.imageView3 = (ImageView) findViewById(R.id.imageView3);
+        this.imageView = (ImageView) findViewById(R.id.imageView);
+        this.imageView2 = (ImageView) findViewById(R.id.imageView2);
     }
 }

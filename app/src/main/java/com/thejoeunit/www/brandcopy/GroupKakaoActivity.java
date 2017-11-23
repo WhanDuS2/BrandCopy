635913c83e2767832821af7d156aa1638d6ba299
cho881020 <cho881020@gmail.com>
2017-11-23 18:06:05 +0900

    포토뷰

--- app/src/main/java/com/thejoeunit/www/brandcopy/GroupKakaoActivity.java
+++ app/src/main/java/com/thejoeunit/www/brandcopy/GroupKakaoActivity.java
@@ -2,6 +2,7 @@ package com.thejoeunit.www.brandcopy;
 
 import android.os.Bundle;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 import com.emrekose.pinchzoom.Touch;
 
@@ -10,6 +11,7 @@ public class GroupKakaoActivity extends BaseActivity {
     private android.widget.ImageView imageView2;
     private android.widget.ImageView imageView;
     private android.widget.ImageView imageView3;
+    private android.widget.LinearLayout layout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,13 +30,12 @@ public class GroupKakaoActivity extends BaseActivity {
 
     @Override
     public void setValues() {
-        imageView.setOnTouchListener(new Touch());
-
     }
 
     @Override
     public void bindViews() {
 
+        this.layout = (LinearLayout) findViewById(R.id.layout);
         this.imageView3 = (ImageView) findViewById(R.id.imageView3);
         this.imageView = (ImageView) findViewById(R.id.imageView);
         this.imageView2 = (ImageView) findViewById(R.id.imageView2);

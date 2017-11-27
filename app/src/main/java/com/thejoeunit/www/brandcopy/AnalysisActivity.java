39c8a066a44f5139935e563da0b13cda3183febe
WhanDooS2 <cjh19890212@gmail.com>
2017-11-27 10:07:31 +0900

    사인업,로그인 XML 작업완료 데이터세팅중 -> 화면넘기기작업까지 완료 가이드 및 가족방 초대 메뉴얼 작업중 스샷으로 사진 다 모아서 줌인기능 추가완료(그룹방) 초보자 가이드 따로 액티비티생성하여 클릭시 포토뷰 되게까지 XML작업완료 리스트뷰 뿌려줄 XML작업완료 및 리스트뷰 액티비티 연결작업중 XML수정완료 -> 게시판글쓰기 작업중 거의 다 되감 각 글쓰는 액티비티 상에 글쓰기 버튼달기중(덴저액티비티에만 시도중)

--- app/src/main/java/com/thejoeunit/www/brandcopy/AnalysisActivity.java
+++ app/src/main/java/com/thejoeunit/www/brandcopy/AnalysisActivity.java
@@ -2,7 +2,9 @@ package com.thejoeunit.www.brandcopy;
 
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Spinner;
 
 import com.thejoeunit.www.brandcopy.adapter.AnalysisAdapter;
 import com.thejoeunit.www.brandcopy.data.PostingData;
@@ -17,6 +19,9 @@ public class AnalysisActivity extends BaseActivity {
     private android.widget.ListView analysisLisTView;
     private android.widget.Button goWrtieBtn;
     private Button wrtieBtn;
+    private ListView analysisListView;
+    private android.widget.Spinner spinner;
+    private android.widget.EditText editText3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,8 +50,9 @@ public class AnalysisActivity extends BaseActivity {
 
     @Override
     public void bindViews() {
-        this.analysisLisTView = (ListView) findViewById(R.id.analysisLisTView);
-        this.goWrtieBtn = (Button) findViewById(R.id.goWrtieBtn);
+        this.editText3 = (EditText) findViewById(R.id.editText3);
+        this.spinner = (Spinner) findViewById(R.id.spinner);
+        this.analysisListView = (ListView) findViewById(R.id.analysisListView);
 
 
     }

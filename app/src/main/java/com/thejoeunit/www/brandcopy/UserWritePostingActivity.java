d78c130082c547ad835d42d64a962b6ee5dc9fa4
WhanDooS2 <cjh19890212@gmail.com>
2017-11-29 17:43:53 +0900

    사인업,로그인 XML 작업완료 데이터세팅중 -> 화면넘기기작업까지 완료 가이드 및 가족방 초대 메뉴얼 작업중 스샷으로 사진 다 모아서 줌인기능 추가완료(그룹방) 초보자 가이드 따로 액티비티생성하여 클릭시 포토뷰 되게까지 XML작업완료 리스트뷰 뿌려줄 XML작업완료 및 리스트뷰 액티비티 연결작업중 XML수정완료 -> 게시판글쓰기 작업중 거의 다 되감 각 글쓰는 액티비티 상에 글쓰기 버튼달기중(덴저액티비티에만 시도중) 메인액티비티 글라이드 통해 이미지 주입 완료 . 각 액티비티 추가 필요 기능 구현중 쓰기버튼 각 액티비티와 연동시키기 각 추가적 부족한 기능구현완료 기본구성완료 및 코딩작업 (DB연동작업중) 서버단 연결 구현중

--- app/src/main/java/com/thejoeunit/www/brandcopy/UserWritePostingActivity.java
+++ app/src/main/java/com/thejoeunit/www/brandcopy/UserWritePostingActivity.java
@@ -1,13 +1,67 @@
 package com.thejoeunit.www.brandcopy;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class UserWritePostingActivity extends AppCompatActivity {
+public class UserWritePostingActivity extends BaseActivity {
+
+    private android.widget.ImageView backIMG;
+    private android.widget.TextView titleTxt;
+    private android.widget.EditText editText;
+    private android.widget.Button enterBtn;
+    private android.widget.Button cancelBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_write_posting);
+
+        bindViews();
+        setupEvents();
+        setValues();
+
+    }
+
+    @Override
+    public void setupEvents() {
+
+        enterBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(mContext, "등록되었습니다.", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(mContext, ServiceCenterActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        backIMG.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+
+
+    }
+
+    @Override
+    public void setValues() {
+
+    }
+
+    @Override
+    public void bindViews() {
+
+        this.cancelBtn = (Button) findViewById(R.id.cancelBtn);
+        this.enterBtn = (Button) findViewById(R.id.enterBtn);
+        this.editText = (EditText) findViewById(R.id.editText);
+        this.titleTxt = (TextView) findViewById(R.id.titleTxt);
+        this.backIMG = (ImageView) findViewById(R.id.backIMG);
     }
 }
